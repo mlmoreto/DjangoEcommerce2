65d42c5076205be5212c1ec244117f7e5f28efce
Guilherme Henrique Lourenço <ghl.guilherme@gmail.com>
2016-05-12 19:09:38 -0300

    desfiz & só funcionou com $

--- snake_shop/urls.py
+++ snake_shop/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^user/', include('user.urls')),
     url(r'^shop/', include('shop.urls')),
-    url(r'^&', include('shop.urls'))## cuidado é & e não $
+    url(r'^$', include('shop.urls'))## cuidado é & e não $-- é $ do outro não roda
 ]
 
 if settings.DEBUG:
