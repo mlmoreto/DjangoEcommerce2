6dac4b340723512ed4e5dd79bbdfeff8a5f87183
cazador1337 <jogadores2001@gmail.com>
2016-05-18 08:44:31 -0300

    ordenação dos jogos

--- shop/views.py
+++ shop/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from shop.models import Genre, Game
 
 def home(request):
-    games = Game.objects.all()
+    games = Game.objects.all().order_by('created').reverse()[:8]
     paginator = Paginator(games, 8) #8 jogos por página
     page = request.GET.get('page')
     try:
