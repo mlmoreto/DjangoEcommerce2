dab39640e0beebaf16a899b4f0615b98e8d6e301
cazador1337 <jogadores2001@gmail.com>
2016-06-02 11:29:57 -0300

    total pedidos

--- history/admin.py
+++ history/admin.py
@@ -1,9 +1,26 @@
 from daterange_filter.filter import DateRangeFilter
 from django.contrib import admin
-
+from django.contrib.admin.views.main import ChangeList
+from django.db.models import Sum, Avg
 from .models import Pedido, Produto
 
 # Register your models here.
+class TotalPedido(ChangeList):
+    fields_to_total = ['total']
+
+    def get_total_values(self, queryset):
+        total = Pedido()
+        total.custom_alias_name = "Total: "
+        for field in self.fields_to_total:
+            print(queryset.aggregate(Sum(field))['total__sum'])
+            setattr(total, field, queryset.aggregate(Sum(field))['total__sum'])
+        return total
+
+    def get_results(self, request):
+        super(TotalPedido, self).get_results(request)
+        total = self.get_total_values(self.get_queryset(request))
+        len(self.result_list)
+        self.result_list._result_cache.append(total)
 
 class PedidoAdmin(admin.ModelAdmin):
     date_hierarchy = 'data'
@@ -13,6 +30,8 @@ class PedidoAdmin(admin.ModelAdmin):
     )
     search_fields = ['data']
 
+    def get_changelist(self, request, **kwargs):
+        return TotalPedido
 
 admin.site.register(Pedido, PedidoAdmin)
 
